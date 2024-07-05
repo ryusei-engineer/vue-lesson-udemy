7482b0fb89f9bbccf96567c5172917f17aecee65
ryusei-engineer <ryusei.engineer@gmail.com>
2024-07-06 01:25:14 +0900

    v-bind v-on

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
-import { ref } from "vue";
-const title = "vue-lesson"
-let int = ref(0)
-function increment() {
+import { ref, reactive } from "vue";
+const message = ref('<h1>hello</h1>')
+
+const URL = ref('https://ja.vuejs.org/')
+const int = ref(0)
+function increment(){
   int.value++
 }
 </script>
 <template>
-<h1>{{ title }}</h1>
-<p>{{ int }}</p>
-<button @click="increment">add</button>
-</template>
+  <p>{{ message }}</p>
+  <p v-html="message"></p>
 
+  <a v-bind:href="URL">Vue.js</a>
+  <p>{{ int }}</p>
+  <button v-on:click="increment">click</button>
+</template>
 <style>
 </style>
