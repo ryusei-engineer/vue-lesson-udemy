c9baa820f12b268e15969e5deb847c8bb38c7b1a
ryusei-engineer <ryusei.engineer@gmail.com>
2024-07-06 21:14:26 +0900

    emit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 import { ref } from "vue";
 import ShowCount from '@/components/ShowCount.vue'
+import ResetButton from '@/components/ResetButton.vue'
 const count = ref(0)
+function reset(value){
+  count.value = value
+}
 </script>
 <template>
 <ShowCount :foo="count" />
 <button @click="count++">add</button>
+<ResetButton  @reset="reset"/>
 </template>
 <style>
 </style>
---
+++ src/components/ResetButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+// import { ref } from "vue";
+defineEmits(['reset'])
+</script>
+<template>
+<button @click="$emit('reset', 100)">reset</button>
+</template>
+<style>
+</style>
